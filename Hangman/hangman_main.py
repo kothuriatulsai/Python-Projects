a123b3ccbdae5fd97f111c82cf76eb5a18d91e54
Atul Sai Kothuri <kothuriatulsai@gmail.com>
2024-01-05 23:32:56 +0530

    Create hangman_main.py
    Hangman Main Code

---
+++ Hangman/hangman_main.py
@@ -0,0 +1,58 @@
+#Step 5
+
+import random
+import hangman_art
+import hangman_words
+
+
+print(hangman_art.logo)
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    
+    if guess in display:
+      print("You have already guessed that letter!")
+    
+
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+        
+        if letter == guess:
+            display[position] = letter
+
+    
+    if guess not in display:
+      print(f"You have guessed the letter {guess}. You lose a life.")
+    if guess not in chosen_word:
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+            print(f"The word was {chosen_word}.")
+
+    
+    
+    print(f"{' '.join(display)}")
+
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    
+    print(hangman_art.stages[lives])
